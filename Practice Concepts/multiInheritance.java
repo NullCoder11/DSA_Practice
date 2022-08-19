7465d9589ba885f20822e16c2fd954abc385e318
Sompadma Mukherjee <99756344+NullCoder11@users.noreply.github.com>
2022-08-20 01:47:43 +0530

    Add files via upload

---
+++ Practice Concepts/multiInheritance.java
@@ -0,0 +1,55 @@
+//basics of inheritance
+
+//creating a super-class(grand-parent class)
+class A{
+int i,j;
+void showij(){
+System.out.println("i and j: "+i+" "+j);
+
+}
+}
+//creating sub-class(child class) by extending class A
+class B extends A{
+int k;
+void showk(){
+System.out.println("k: "+k);
+}
+void sum(){
+System.out.println("i+j+k: "+(i+j+k));
+}
+}
+//creating another sub-class by extending class B
+class C extends B{
+int l,m;
+void showlm(){
+System.out.println("l and m: "+l+""+m);
+}
+void print_fun(){
+System.out.println("I cannot inherit class A directly!!");
+}
+}
+class multiInheritance{
+public static void main(String[]args){
+A superOb = new A();
+B subOb = new B();
+C multiOb = new C();
+
+superOb.i = 10;
+superOb.j = 20;
+System.out.println("Contents of superOb: ");
+superOb.showij();
+System.out.println();
+
+subOb.i=7;
+subOb.j=8;
+subOb.k=9;
+System.out.println("Contents of subOb: ");
+subOb.showij();
+subOb.showk();
+System.out.println();
+
+System.out.println("Sum of i,j and k in subOb: ");
+subOb.sum();
+multiOb.print_fun();
+}
+}
