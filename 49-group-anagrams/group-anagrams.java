701fbbc8447627810f529bbcb132812ab17f1b4c
Sompadma Mukherjee <99756344+NullCoder11@users.noreply.github.com>
2023-10-26 13:26:54 +0530

    Time: 5 ms (98.79%) | Memory: 47.3 MB (22.59%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,25 +1,29 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        if(strs==null||strs.length==0){
-            return new ArrayList<>();
-        }
-        //map used to store the groups of anagrams
-        Map<String,List<String>>map = new HashMap<>();
-        for(String s:strs){
-            //for each string converts it to the character array
-            char[]charArray = s.toCharArray();
-            //after conversion sorts the array
-            Arrays.sort(charArray);
-            //creating a new string from sorted array which acts as the key for grouping
-            String sorted = new String(charArray);
-            //checking whether sorted string exists as a key in the map
-            if(!map.containsKey(sorted)){
-                //if it dosen't 
-                map.put(sorted,new ArrayList<>());
-            }
-            map.get(sorted).add(s);
-
+       if(strs==null||strs.length==0) {
+           return new ArrayList<>();
+       }
+       //map used to store the group of anagrams
+           //key  value        pair
+       Map<String,List<String>>map = new HashMap<>();
+       for(String s:strs){
+           //converting each string to char array
+           char[]arr = s.toCharArray();
+           //after conversion sorting the array
+           Arrays.sort(arr);
+           //creating a new string from sorted arr which acts as the key for grouping
+           String sorted = new String(arr);  
+           //checking whether sorted string exists as a key in the map
+           if(!map.containsKey(sorted)){
+               //if not
+               map.put(sorted,new ArrayList<>());
+           }
+           map.get(sorted).add(s);
+           
+           
         }
         return new ArrayList<>(map.values());
+
+
     }
 }
